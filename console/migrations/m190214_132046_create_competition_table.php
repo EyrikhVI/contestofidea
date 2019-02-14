b7512fcca28c96ee3b94d51ff539d4ca58942175
EyrikhVI <eyrikh@mail.ru>
2019-02-15 00:21:49 +1000

    Создание таблицы competition от 15.02.2019

---
+++ console/migrations/m190214_132046_create_competition_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `competition`.
+ */
+class m190214_132046_create_competition_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%competition}}', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer()->notNull()->comment('Владелец конкурса - id из таблицы user'),
+            'name' => $this->string()->notNull()->comment('Наименование конкурса'),
+            'note' => $this->string()->notNull()->comment('Аннотация конкурса'),
+            'start_date' => $this->integer()->notNull()->comment('Дата/время начала конкурса'),
+            'application_start_date' => $this->integer()->notNull()->comment('Дата/время начала приема заявок'),
+            'application_end_date' => $this->integer()->notNull()->comment('Дата/время окончания приема заявок'),
+            'end_date' => $this->integer()->notNull()->comment('Дата/время окончания конкурса'),
+            'application_for_participant' => $this->smallinteger()->notNull()->defaultValue(1)->
+            comment('Кол-во заявок от участника'),
+            'application_for_competition' => $this->smallinteger()->notNull()->defaultValue(0)->
+            comment('Кол-во заявок конкурс'),
+            'views_for_competition' => $this->smallinteger()->notNull()->defaultValue(0)->
+            comment('просмотров конкурса'),
+            'status' => $this->smallInteger()->notNull()->defaultValue(0)->comment('Статус конкурса'),
+            'created_at' => $this->integer()->notNull()->comment('Дата/время создания конкурса'),
+            'updated_at' => $this->integer()->notNull()->comment('Дата/время обновления конкурса'),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%competition}}');
+    }
+}
