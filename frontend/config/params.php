28ba60206e9902f5535206cb78a82019ce93f7ca
EyrikhVI <eyrikh@mail.ru>
2019-04-19 22:06:08 +1000

    Revert "Создание БД Content от 26.03.2019"
    This reverts commit 36c526d

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -2,16 +2,5 @@
 return [
     'adminEmail' => 'admin@example.com',
     'UploadAvatar' => '@web/uploads/avatar/', // путь хранения файлов изображений аватаров
-    'NoImageAvatar'=>'no_photo_available.jpg', //Имя файла изображения пустого аватара
-    Yii::setAlias('common', dirname(__DIR__)),
-    Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend'),
-    Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend'),
-    Yii::setAlias('menacore',dirname(dirname(__DIR__))),
-    Yii::setAlias('menaBase',dirname(dirname(dirname(__DIR__)))),
-    Yii::setAlias('menaThemes',dirname(dirname(dirname(__DIR__))).'/themes'),
-    Yii::setAlias('upload',dirname(dirname(dirname(__DIR__))).'/upload'),
-    Yii::setAlias('themes',dirname(dirname(dirname(__DIR__))).'/themes'),
-    Yii::setAlias('blocks',dirname(dirname(dirname(__DIR__))).'/blocks'),
-    Yii::setAlias('menaBasePublic',substr(str_replace('\\', '/', realpath(dirname(dirname(dirname(__DIR__))))), strlen(str_replace('\\', '/', realpath($_SERVER['DOCUMENT_ROOT']))))),
-
+    'NoImageAvatar'=>'no_photo_available.jpg' //Имя файла изображения пустого аватара
 ];
