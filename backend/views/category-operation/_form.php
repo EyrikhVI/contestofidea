7a8e9064657c511c77a1800039c21aacc4f69301
EyrikhVI_work <eyrikh@mail.ru>
2019-05-21 17:13:04 +1000

    Добавление полей в competition от 21.05.2019

--- backend/views/category-operation/_form.php
+++ backend/views/category-operation/_form.php
@@ -13,9 +13,9 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
 
 <!--    --><?//= $form->field($model, 'parent_id')->textInput() ?>
-    <div class="form-group field-category-parent_id has-success">
-    <label class="control-label" for="category-parent_id">Родительская категория</label>
-    <select id="category-parent_id" class="form-control" name="Category[parent_id]">
+    <div class="form-group field-categoryoperation-parent_id has-success">
+    <label class="control-label" for="categoryoperation-parent_id">Родительская категория</label>
+    <select id="categoryoperation-parent_id" class="form-control" name="CategoryOperation[parent_id]">
         <option value="0">-</option>
     <?= \frontend\components\MenuWidget::widget(['tpl'=>'select','model'=>$model])?>
     </select>
